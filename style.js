ac31cf87ba7cd838dcdd77b6d74bf20e2be25254
DimasMukhlasIbrahim <dimasmuklas123@gmail.com>
2023-04-18 17:37:50 +0700

    web kedai kopi kenangan senja

---
+++ style.js
@@ -0,0 +1,36 @@
+// // toggle class active
+// const button = document.querySelector ('#hamburger');
+// const sidebar = document.querySelector ('navbar-nav-open')
+// // ktk di kclick
+// button .addEventListener("click", () => {
+//     sidebar.classList.button("navbar-nav-open");
+//   });
+
+
+
+// const menuToggle = document.querySelector('.menu-toggle');
+// const sidebar = document.querySelector('.sidebar');
+
+// function openSidebar() {
+//  let = ('#hamburger') =  document.getElementById("#hamburger").style.right = "0";
+//  #hamburger.src = #NavbarNav
+//   }
+  
+//   function closeSidebar() {
+//     document.getElementById("sidebar").style.right = "-250px";
+//   }
+ 
+
+// const toggle = document.getElementById ('hamburger')
+// const sidebar = document.getElementsByClassName ('navbar-nav') [0]
+
+// toggle.addEventListener ('click', function () {
+//     sidebar.classList.add ('hide ')
+// })
+
+
+// $(document).ready(function(){
+// 	$('.tombol').click(function(){
+// 		$('.navbar-nav').toggleClass(".slide-navbar-nav-tampil");
+// 	});
+// });
